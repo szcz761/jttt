7e00190a1cf83dfd87ffcfa0e883e2617bd7b648
szymon <szcz761@gmail.com>
2018-03-21 20:31:06 +0100

    nana

--- JTTT/MainWindow.xaml.cs
+++ JTTT/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
 using System.IO;
-using HtmlAgilityPack;
-using System.Text;
 
 
 
@@ -32,20 +18,16 @@ namespace JTTT
         {
             InitializeComponent();
             StreamWriter log = File.AppendText("JTTT.log");
-            log.WriteLine(DateTime.Now.ToString() + "====================== Uruchomienie programu ======================");
+            log.WriteLine(DateTime.Now.ToString() + " ====================== Uruchomienie programu ======================");
             log.Close();
         }
     
 
     private void Button_Wykonaj_Click(object sender, RoutedEventArgs e)
         {
-            Task task = new Task(Textbox_Text.Text, Textbox_URL.Text, Textbox_URL.Text);
+            Task task = new Task(Textbox_Text.Text, Textbox_URL.Text, Textbox_Mail.Text);
             ConsoleTextbox.Text = task.process();
         }
 
-        private void Textbox_Text_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
